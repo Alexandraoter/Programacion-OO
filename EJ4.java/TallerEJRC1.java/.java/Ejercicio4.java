f356385f44e0d696e089db02e0fb7ecef36be783
Alexandraoter <148839833+Alexandraoter@users.noreply.github.com>
2024-09-03 20:26:59 +0000

    Falta el ejercicio 5

--- EJ4.java/TallerEJRC1.java/.java/Ejercicio4.java
+++ EJ4.java/TallerEJRC1.java/.java/Ejercicio4.java
@@ -1,3 +1,15 @@
 public class Ejercicio4 {
-    
+    public static void main(String[] args) {
+        perfume objPerfume1 = new perfume("Coconut Passion", "Victoria Secret", 150, 80.000);
+        perfume objPerfume2 = new perfume("Hugo boss", "Bottled Absolu", 200, 664.677);
+
+
+        objPerfume1.aplicarPerfume(50);
+        objPerfume1.consultarCantidadRestante();
+        objPerfume1.ajustarPrecio(200000, 150);
+
+        objPerfume2.aplicarPerfume(75);
+        objPerfume2.consultarCantidadRestante();
+        objPerfume2.ajustarPrecio(670000, 200);
+    }
 }
--- EJ4.java/TallerEJRC1.java/.java/perfume.java
+++ EJ4.java/TallerEJRC1.java/.java/perfume.java
@@ -1,4 +1,34 @@
 public class perfume {
+
     private String nombre;
     private String marca;
+    private double capacidad;
+    private double precio;
+    
+    public perfume( String nombre, String marca, double capacidad, double precio){
+        this.nombre = nombre;
+        this.marca = marca;
+        this.capacidad = capacidad;
+        this.precio = precio;
+    }
+
+    public void aplicarPerfume(double cantidad){
+        if (cantidad <= capacidad){
+            capacidad-= cantidad;
+        }else{
+            System.out.println("No hay suficiente perfume para aplicar");
+        }
+    }
+
+    public void consultarCantidadRestante(){
+        System.out.print("La cantidad restante en el perfume " + nombre + "es de " + capacidad + ".");
+    }
+
+    public void ajustarPrecio(double nuevoPrecio, double cantidad){
+        if (cantidad <= precio){
+            precio = nuevoPrecio;
+        }else{
+            System.out.println(" Se ha actualizado el precio de " + nombre + "ahora es: " + nuevoPrecio + ".");
+        }
+    }
 }
