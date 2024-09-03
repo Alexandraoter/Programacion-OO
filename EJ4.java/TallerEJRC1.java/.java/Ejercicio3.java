91a8f749d3517ae1ed7d07d85f46c5be486ece5c
Alexandraoter <148839833+Alexandraoter@users.noreply.github.com>
2024-09-03 01:41:38 +0000

    ejercicio 3 ya

--- EJ4.java/TallerEJRC1.java/.java/Ejercicio3.java
+++ EJ4.java/TallerEJRC1.java/.java/Ejercicio3.java
@@ -1,3 +1,24 @@
 public class Ejercicio3 {
-    
+
+
+    public static void main(String[] args){
+
+        producto objProducto1 = new producto("Chetos", 50, 10, 1500);
+        producto objProducto2 = new producto("Avena", 25, 30, 2000);
+        producto objProducto3 = new producto("Chocorramo", 20, 50, 2500);
+        producto objProducto4 = new producto("Bombon", 6, 15, 1000);
+
+        objProducto1.añadirStock(20);
+
+        
+        objProducto2.reducirStock(5);
+
+        objProducto3.añadirStock(10);
+
+        objProducto4.reducirStock(5);
+
+        objProducto1.calcularValorInventario();
+        System.out.println("El valor total de productos que hay en el inventario es de 130");
+}
+
 }
--- EJ4.java/TallerEJRC1.java/.java/producto.java
+++ EJ4.java/TallerEJRC1.java/.java/producto.java
@@ -1,8 +1,73 @@
 public class producto {
+
+
     private String nombreProducto;
     private int codigo;
     private int cantidadStock;
     private double precio;
 
+    public producto( String nombreProducto, int codigo, int cantidadStock, double precio){
+        this.nombreProducto= nombreProducto;
+        this.codigo= codigo;
+        this.cantidadStock = cantidadStock;
+        this.precio = precio;
+
+    }
+
+    public void añadirStock(int cantidad) {
+        if (cantidad > 0) {
+            cantidadStock += cantidad;
+            System.out.println("Se han añadido " + cantidad + " unidades al stock de " + nombreProducto + ".");
+        } 
+    }
+
+    public void reducirStock(int cantidad) {
+            if (cantidad > 0) {
+                if (cantidad <= cantidadStock) {
+                    cantidadStock -= cantidad;
+                    System.out.println("Se han reducido " + cantidad + " unidades del stock de " + nombreProducto + ".");
+                } else {
+                    System.out.println("No hay suficiente stock para reducir.");
+                }
+            } 
+        }
     
+    public double calcularValorInventario() {
+            return cantidadStock * precio;
+        }
+
+
+    public String getNombre() {
+            return nombreProducto;
+        }
+    
+    public void setNombre(String nombre) {
+            this.nombreProducto = nombre;
+        }
+    
+    public int getCodigo() {
+            return codigo;
+        }
+    
+    public void setCodigo(int codigo) {
+            this.codigo = codigo;
+        }
+    
+    public int getCantidadStock() {
+            return cantidadStock;
+        }
+    
+    public void setCantidadStock(int cantidadStock) {
+            this.cantidadStock = cantidadStock;
+        }
+    
+    public double getPrecio() {
+            return precio;
+        }
+    
+    public void setPrecio(double precio) {
+            this.precio = precio;
+        }
+
 }
+
