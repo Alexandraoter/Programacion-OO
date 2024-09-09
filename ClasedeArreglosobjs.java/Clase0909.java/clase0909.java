df4c318d9f339d29835d47597c6b3053eff728c9
Alexandraoter <148839833+Alexandraoter@users.noreply.github.com>
2024-09-09 23:56:58 +0000

    Primera clase primer ejemplo

---
+++ ClasedeArreglosobjs.java/Clase0909.java/clase0909.java
@@ -0,0 +1,11 @@
+public class clase0909 {
+    public static void main(String[] args){
+
+        //opcion1 creacion de arreglo de enteros
+        int [] a = {6, 20, 31, 16};
+        //siempre se recorre con un for
+        for(int i = 0; i < a.length; i++){
+            System.out.println("a["+ i + "] = " + a[i]);
+        }
+    }
+}
