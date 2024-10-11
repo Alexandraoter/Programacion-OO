c1d95bdb718bffd4f3a2f861b35969d603dd8c45
Alexandraoter <148839833+Alexandraoter@users.noreply.github.com>
2024-10-11 18:58:14 +0000

    Todo listo

--- Momento2.java/Main.java
+++ Momento2.java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
@@ -6,14 +7,17 @@ public class Main {
         Foto foto = new Foto(201, "Foto de paisaje", "Foto de alta resolución", 50.00, 5, 1080, "JPEG");
 
         // Crear una impresión a partir de la foto
-        Impresion impresionEnmarcada = new Impresion(2, "Impresión enmarcada", "Paisaje en marco", 30.99, 5, new Date(), fotoPaisaje, 11, "Fotográfico", 10);
+        Impresion impresionEnmarcada = new Impresion(2, "Impresión enmarcada", "Paisaje en marco", 30.99, 5, new Date(), foto, 11, "Fotográfico", 10);
 
         // Crear un cliente
         Cliente cliente = new Cliente(25, "Carlos", "Campo", "Carrera Ac1");
 
+        // Crear una lista de detalles de pedido
+        ArrayList<DetallePedido> detalles = new ArrayList<>();
+
         // Crear un pedido
         Pedido pedido = new Pedido(1, "Juan", "Pérez", "Calle Falsa 123", "123456789", "juan@example.com", 1001, cliente, detalles);
-        pedido.mostrarDetalles();
+
         // Agregar la impresión al pedido
         DetallePedido detalleImpresion = new DetallePedido(impresionEnmarcada, 2);
         pedido.agregarDetalle(detalleImpresion);
@@ -23,3 +27,4 @@ public class Main {
         pedido.mostrarDetalles();
     }
 }
+
